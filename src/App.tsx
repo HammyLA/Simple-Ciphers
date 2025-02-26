cbd9b7a4acda753c22fb551a17a4c29b761f40da
Lawrence <cuencoandrei@gmail.com>
2025-02-26 14:15:07 -0800

    format fix
    centered properly, max-width is now properly set, goes to home page now.

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
+        <Route path="/Simple-Ciphers" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="/ciphers" element={<Ciphers />} />
         <Route path="/about" element={<About />} />
