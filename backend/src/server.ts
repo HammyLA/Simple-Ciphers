b7281ae6e0f2d55c26d759fda506017c63870146
Lawrence <131471451+HammyLA@users.noreply.github.com>
2025-03-08 21:24:42 -0800

    trying to prepare backend for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,24 +5,9 @@ import authMiddleware from '../middleware/authMiddleware';
 import usersRoutes from '../routes/usersRoutes';
 import keysRoutes from '../routes/keysRoutes';
 import globalStatisticsRoutes from '../routes/globalStatisticsRoutes';
-import { exec } from 'child_process';
 
 const app = express();
 const PORT = process.env.PORT || 5003
-
-app.post('/migrate', async (req, res) => {
-  try {
-    // Run Prisma migrate deploy to apply migrations
-    exec('npx prisma migrate dev --name init', (err, stdout, stderr) => {
-      if (err) {
-        return res.status(500).send(`Migration Error: ${stderr}`);
-      }
-      res.status(200).send(`Migration successful: ${stdout}`);
-    });
-  } catch (error) {
-    res.status(500).send('Error triggering migration');
-  }
-});
 
 // Middleware to have the server use json
 app.use(express.json())
